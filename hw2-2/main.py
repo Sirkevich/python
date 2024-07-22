43456daf61713ae4618cab04537076b4acb489da
Ihor Sirko <62531809+Sirkevich@users.noreply.github.com>
2024-07-22 09:50:51 +0300

    Update main.py

--- hw2-2/main.py
+++ hw2-2/main.py
@@ -4,6 +4,6 @@ first_digit = str(user_number // 10000)
 second_digit = str(user_number % 10000 // 1000)
 third_digit = str(user_number % 1000 // 100)
 fourth_digit = str(user_number % 100 // 10)
-five_digit = str(user_number % 10)
+fifth_digit = str(user_number % 10)
 
-print(five_digit + fourth_digit + third_digit + second_digit + first_digit)
+print(fifth_digit + fourth_digit + third_digit + second_digit + first_digit)
