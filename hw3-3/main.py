f3df4276828fa19bea60778d384f11e9efc3fb39
Ihor Sirko <sirkewich92@gmail.com>
2024-07-26 18:10:32 +0300

    list division

---
+++ hw3-3/main.py
@@ -0,0 +1,15 @@
+user_list = []
+result_list = []
+
+middle_of_list = int(len(user_list) / 2)
+
+if len(user_list) == 0:
+    result_list = [[], []]
+
+elif len(user_list) % 2 == 0:
+    result_list = [user_list[:middle_of_list], user_list[middle_of_list:]]
+
+else:
+    result_list = [user_list[:middle_of_list + 1], user_list[middle_of_list + 1:]]
+
+print(result_list)
