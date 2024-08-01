bde4fa3b8c09f16aa5c37c29f7bf481d6327d9bb
Ihor Sirko <sirkewich92@gmail.com>
2024-08-01 18:29:01 +0300

    feat: variable name validation

---
+++ hw5-1/main.py
@@ -0,0 +1,34 @@
+import keyword
+import string
+
+user_string = input("Enter your string: ")
+
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+string_punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^`{|}~"""
+count = 0
+
+result = True
+
+for elem in user_string:
+
+    if user_string in keyword.kwlist:
+        result = False
+        break
+
+    if user_string[0] in numbers:
+        result = False
+        break
+
+    if elem == elem.capitalize() and False == (elem in numbers) and False == (elem in string.punctuation):
+        result = False
+        break
+
+    if user_string.count('_') > 1:
+        result = False
+        break
+
+    if elem in string_punctuation:
+        result = False
+        break
+
+print(result)
