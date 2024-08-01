d820f2f6f7f87343f2ff470036741ea78e5f5c1b
sirkevich <sirkewich92@gmail.com>
2024-08-01 23:00:23 +0300

    feat: add checking on symbol '_'

--- hw5-1/main.py
+++ hw5-1/main.py
@@ -11,19 +11,18 @@ result = True
 
 for elem in user_string:
 
-    if user_string in keyword.kwlist:
-        result = False
-        break
-
     if user_string[0] in numbers:
         result = False
         break
 
-    if elem == elem.capitalize() and False == (elem in numbers) and False == (elem in string.punctuation):
+    if user_string in keyword.kwlist:
         result = False
         break
 
-    if user_string.count('_') > 1:
+    if len(user_string) > 1 and user_string[1] == '_':
+        result = False
+
+    if elem == elem.capitalize() and False == (elem in numbers) and False == (elem in string.punctuation):
         result = False
         break
 
