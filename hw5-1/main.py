f4d11f7bc5b098e79a20bc65cb2c698d16ff9e28
Ihor Sirko <sirkewich92@gmail.com>
2024-08-02 10:42:44 +0300

    refactor: optimization code

--- hw5-1/main.py
+++ hw5-1/main.py
@@ -4,29 +4,26 @@ import string
 user_string = input("Enter your string: ")
 
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-string_punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^`{|}~"""
-count = 0
+my_string_punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^`{|}~"""
 
 result = True
 
+if user_string[0] in numbers:
+    result = False
+
+if user_string in keyword.kwlist:
+    result = False
+
+if len(user_string) > 1 and user_string[1] == '_':
+    result = False
+
 for elem in user_string:
 
-    if user_string[0] in numbers:
+    if elem in my_string_punctuation:
         result = False
         break
-
-    if user_string in keyword.kwlist:
-        result = False
-        break
-
-    if len(user_string) > 1 and user_string[1] == '_':
-        result = False
 
     if elem == elem.capitalize() and False == (elem in numbers) and False == (elem in string.punctuation):
-        result = False
-        break
-
-    if elem in string_punctuation:
         result = False
         break
 
