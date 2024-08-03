76123a870715306b337b8f3ea9736a49502f49e6
sirkevich <sirkewich92@gmail.com>
2024-08-03 21:15:52 +0300

    feat: create hashtag

--- hw5-3/main.py
+++ hw5-3/main.py
@@ -1,25 +1,12 @@
 import string
 
-user_string = ('This is a sample string that contains exactly one hundred and forty characters. It includes spaces and '
-               'punctuation as part of the total character cot.This is a sample string that contains exactly one '
-               'hundred and  forty characters. It includes spaces and'
-               'punctuation as part of the total character cot.')
-# input("Enter your string: "))
-print(len(user_string))
-data = user_string.split()
+user_string = input("Enter your string: ")
+user_string = user_string.title()
 
-result_string = ''
-
-for elem in data:
-    elem = elem.capitalize()
-    result_string += elem
-
-for elem in result_string:
+for elem in user_string:
     if elem in string.punctuation or elem == ' ':
-        result_string = result_string.replace(elem, '')
+        user_string = user_string.replace(elem, '')
     else:
         continue
 
-print(data)
-print(len(result_string))
-print(f'#{result_string}')
+print(f'#{user_string[0:140]}')
