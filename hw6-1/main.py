540ddf95a78d4571dbe18da20477cf6e2f04e4f4
Ihor Sirko <sirkewich92@gmail.com>
2024-08-08 10:32:56 +0300

    feat: range of letters

--- hw6-1/main.py
+++ hw6-1/main.py
@@ -2,39 +2,7 @@ import string
 
 user_letters = input('Enter your letters: ')
 
-lower_letters = 'abcdefghijklmnopqrstuvwxyz'
-upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+first_letter = user_letters[0]
+last_letter = user_letters[-1]
 
-
-user_letters = user_letters.split('-')
-user_letters = ''.join(user_letters)
-
-
-first_letter = ord(user_letters[0])
-second_letter = ord(user_letters[1])
-result = ''
-
-if first_letter < second_letter:
-    while first_letter <= second_letter:
-        result += (chr(first_letter))
-        first_letter += 1
-
-else:
-    i = 65
-    while i <= second_letter:
-        result += (chr(i))
-        i += 1
-    result = lower_letters + result
-
-print(result)
-
-print(string.ascii_letters)
-a
-print(ord('a'))
-print(ord('Z'))
-# for let in string.ascii_letters:
-#     i = user_letters[0]
-#     if let !=
-#     result = ''
-
-
+print(string.ascii_letters[string.ascii_letters.index(first_letter):string.ascii_letters.index(last_letter) + 1])
