27e756dcafde40a88bbc9df8e3fafc485563f5d2
Ihor Sirko <sirkewich92@gmail.com>
2024-08-10 13:00:34 +0300

    feat: add return

--- hw7-1/main.py
+++ hw7-1/main.py
@@ -1,5 +1,7 @@
 def say_hi(name, age):
-    print(f"Hi. My name is {name} and I'm {age} years old")
+    return f"Hi. My name is {name} and I'm {age} years old"
 
 
-say_hi("Alex", 32)
+result = say_hi("Alex", 32)
+
+print(result)
