2000c93aa2e54555a504e23ce24ccbd0da5d6d7d
Ihor Sirko <sirkewich92@gmail.com>
2024-08-17 12:24:21 +0300

    feat: add popular_words func

--- hw9-1/main.py
+++ hw9-1/main.py
@@ -1,1 +1,21 @@
+def popular_words(text: str, words: list) -> dict:
+    """
+    Search chosen words in text
 
+    :param text: some text
+    :param words: list with chosen words
+    :return: dictionary with count founded word
+    """
+    text_list = text.lower().split()
+    res_dict = {}
+
+    for elem in words:
+        res_dict[elem] = text_list.count(elem)
+
+    return res_dict
+
+
+result = popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                       ['i', 'was', 'three', 'near'])
+
+print(result)
