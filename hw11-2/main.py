e2bd21ffe93b972a26b5faa41506feeedfbd43ac
Ihor Sirko <sirkewich92@gmail.com>
2024-08-27 12:32:23 +0300

    refactor: apply prettier

--- hw11-2/main.py
+++ hw11-2/main.py
@@ -2,19 +2,21 @@ from inspect import isgenerator
 
 
 def generate_cube_numbers(end):
-    for elem in range(2, end + 1):
-        elem_cube = elem ** 3
 
-        if elem_cube <= end:
-            yield elem_cube
-        else:
+    current_number = 2
+    while True:
+        if current_number ** 3 > end:
             return
+        yield current_number ** 3
+
+        current_number += 1
 
 
-gen = generate_cube_numbers(1)
+gen = generate_cube_numbers(1000)
 
 assert isgenerator(gen) == True, 'Test0'
 assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
 assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
 assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+
 print('ok')
