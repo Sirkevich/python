934cb614d0352c321da28db55b5620da80a84226
Ihor Sirko <sirkewich92@gmail.com>
2024-08-28 15:59:22 +0300

    feat: add delete_html_tags func

--- hw12-1/main.py
+++ hw12-1/main.py
@@ -1,17 +1,18 @@
 import codecs
 import re
 
+
 def delete_html_tags(html_file, result_file='cleaned.txt'):
     with codecs.open(html_file, 'r', 'utf-8') as file:
         html = file.read()
-        html_copy = html
+        html_list = re.findall(r'(<.*?>)', html, re.DOTALL)
 
-        html_list = re.findall(r'<(.*?)>', html)
-        print(html_list)
+        for elem in html_list:
+            if elem in html:
+                html = html.replace(elem, '')
 
     with codecs.open(result_file, 'w', 'utf-8') as file:
-
-        file.write('hello world')
+        file.write(html)
 
 
 delete_html_tags('draft.html')
