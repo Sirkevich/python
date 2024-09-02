22b3bb2eb01f92fb189b83d968e36601f57cf058
Ihor Sirko <sirkewich92@gmail.com>
2024-09-02 16:45:52 +0300

    feat: find_student method

--- hw13-1/main.py
+++ hw13-1/main.py
@@ -20,10 +20,10 @@ class Student(Human):
 
     def __str__(self):
         return (f"Student:"
-                f" {self.first_name} {self.last_name}\n"
+                f"{self.first_name} {self.last_name}\n"
                 f"Gender: {self.gender}\n"
                 f"Age: {self.age}\n"
-                f"Record book: {self.record_book}")
+                f"Record book: {self.record_book}\n\n")
 
 
 class Group:
@@ -36,40 +36,46 @@ class Group:
         self.group.add(student)
 
     def delete_student(self, last_name):
-        pass
+        res = self.find_student(last_name)
+        if res in self.group:
+            self.group.remove(res)
+        else:
+            return print("Student not in the group")
 
     def find_student(self, last_name):
-        pass
+
+        for elem in list(self.group):
+            if last_name in str(elem):
+                return print(elem)
+            else:
+                return None
 
     def __str__(self):
         all_students = ''
-        ...
-        return f'Number:{self.number}\\n {all_students} '
+        for elem in self.group:
+            all_students += str(elem)
 
-
-worker = Human('male', 31, 'Ihor', 'Sirko')
-
-st1 = Student('male', 20, 'Stepan', 'Bandera', 'A')
-
-gr = Group('PD1')
-
-gr.add_student(st1)
-
-print(gr.group)
-
-print(worker)
-print('-' * 100)
-
-print(st1)
-print('-' * 100)
+        return (f'Number:{self.number}'
+                f'\n{all_students}')
 
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('Male', 21, 'Tim', 'Kuk', 'AN146')
 
 gr = Group('PD1')
 
 gr.add_student(st1)
 gr.add_student(st2)
+gr.add_student(st3)
 
 print(gr)
+
+gr.find_student('Jobs')
+
+# gr.delete_student('Taylor')
+#
+# print(gr)
+
+# print(str(gr.find_student('Jobs2')) == str(st1))
+# print(isinstance(gr.find_student('Jobs'), Student))
