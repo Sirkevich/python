2f0972d49b4e656b79c48139af88078d65121ff8
Ihor Sirko <sirkewich92@gmail.com>
2024-09-03 11:31:26 +0300

    feat: add Counter class

--- hw13-2/main.py
+++ hw13-2/main.py
@@ -1,47 +1,56 @@
 class Counter:
 
-   def __init__(self, current=1, min_value=0, max_value=10):
-       self.current = current
-       self.min_value = min_value
-       self.max_value = max_value
+    def __init__(self, current=1, min_value=0, max_value=10):
+        self.current = current
+        self.min_value = min_value
+        self.max_value = max_value
 
-   def set_current(self, start):
-       self.current = start
+    def set_current(self, start):
+        self.current = start
 
-   def set_max(self, max_max):
-        pass
+    def set_max(self, max_max):
+        self.max_value = max_max
 
-   def set_min(self, min_min):
-       pass
+    def set_min(self, min_min):
+        self.min_value = min_min
 
-   def step_up(self):
-       pass
+    def step_up(self):
+        if self.current != self.max_value:
+            self.current += 1
+        else:
+            raise ValueError("Counter has maximum Value")
 
-   def step_down(self):
-       pass
+    def step_down(self):
+        if self.current != self.min_value:
+            self.current -= 1
+        else:
+            raise ValueError("Counter has minimum Value")
 
-   def get_current(self):
-       return self.current
+    def get_current(self):
+        return self.current
+
 
 counter = Counter()
 counter.set_current(7)
 counter.step_up()
 counter.step_up()
 counter.step_up()
+
 assert counter.get_current() == 10, 'Test1'
 try:
-    counter.step_up()  # ValueError
+    counter.step_up()
 except ValueError as e:
-    print(e) # Достигнут максимум
+    print(e)
 assert counter.get_current() == 10, 'Test2'
 
 counter.set_min(7)
 counter.step_down()
 counter.step_down()
 counter.step_down()
+
 assert counter.get_current() == 7, 'Test3'
 try:
-    counter.step_down()  # ValueError
+    counter.step_down()
 except ValueError as e:
-    print(e) # Достигнут минимум
+    print(e)
 assert counter.get_current() == 7, 'Test4'
