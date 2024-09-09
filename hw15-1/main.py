66f6466a63e42ed63470bf4f6fae3ed04c787975
Ihor Sirko <sirkewich92@gmail.com>
2024-09-09 11:07:29 +0300

    feat: add __add__, __mul__, __str__ methods

--- hw15-1/main.py
+++ hw15-1/main.py
@@ -11,30 +11,31 @@ class Rectangle:
         return self.get_square() == other.get_square()
 
     def __add__(self, other):
-        return self.get_square() + other.get_square()
+        sum_area = self.get_square() + other.get_square()
+        return Rectangle(self.width, sum_area // self.width)
 
     def __mul__(self, n):
-        self.get_square() * n
+        new_area = self.get_square() * n
+        return Rectangle(self.width, new_area // self.width)
 
     def __str__(self):
-        pass
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 
 r1 = Rectangle(2, 4)
 r2 = Rectangle(3, 6)
 
-print(r1 == r2)
-print(r1 + r2)
-print(r1 * 4)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
 
-print(Rectangle(3, 6) == Rectangle(2, 9))
-# assert r1.get_square() == 8, 'Test1'
-# assert r2.get_square() == 18, 'Test2'
-#
-# r3 = r1 + r2
-# assert r3.get_square() == 26, 'Test3'
-#
-# r4 = r1 * 4
-# assert r4.get_square() == 32, 'Test4'
-#
-# assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+r3 = r1 + r2
+
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+
+assert r4.get_square() == 32, 'Test4'
+
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+
+print(r4)
