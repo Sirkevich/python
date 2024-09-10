ae200c8b60947957eeeba721755c42ddec1660e7
Ihor Sirko <sirkewich92@gmail.com>
2024-09-10 10:59:52 +0300

    refactor: apply prettier

--- hw15-1/main.py
+++ hw15-1/main.py
@@ -12,30 +12,36 @@ class Rectangle:
 
     def __add__(self, other):
         sum_area = self.get_square() + other.get_square()
-        return Rectangle(self.width, sum_area // self.width)
+
+        new_width = self.width
+
+        new_height = sum_area // new_width
+
+        while new_width * new_height != sum_area:
+            new_width -= 1
+            new_height = sum_area // new_width
+
+        return Rectangle(new_width, new_height)
 
     def __mul__(self, n):
         new_area = self.get_square() * n
+
         return Rectangle(self.width, new_area // self.width)
 
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
 
 
-r1 = Rectangle(2, 4)
-r2 = Rectangle(3, 6)
-
-assert r1.get_square() == 8, 'Test1'
-assert r2.get_square() == 18, 'Test2'
+r1 = Rectangle(25, 7)
+r2 = Rectangle(23, 124)
+assert r1.get_square() == 175, 'Test1'
+assert r2.get_square() == 2852, 'Test2'
 
 r3 = r1 + r2
+assert r3.get_square() == 3027, 'Test3'
 
-assert r3.get_square() == 26, 'Test3'
+r4 = r3 * 4
 
-r4 = r1 * 4
-
-assert r4.get_square() == 32, 'Test4'
+assert r4.get_square() == 12108, 'Test4'
 
 assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
-
-print(r4)
